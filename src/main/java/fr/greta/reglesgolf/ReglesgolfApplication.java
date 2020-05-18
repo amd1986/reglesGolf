f7253c771548d288e90b8f8a69c5cba4cc03e0d0
BEN THAIER Ahmed <benthaier.ahmed@gmail.com>
2020-05-18 16:56:33 +0200

    First commit : generate Spring Boot projet:

---
+++ src/main/java/fr/greta/reglesgolf/ReglesgolfApplication.java
@@ -0,0 +1,13 @@
+package fr.greta.reglesgolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReglesgolfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReglesgolfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/greta/reglesgolf/ReglesgolfApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.greta.reglesgolf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReglesgolfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
